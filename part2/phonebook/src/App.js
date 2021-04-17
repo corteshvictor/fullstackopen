8b8a45a547c3a37bebea4766104014c58aa96806
Victor Cortés <cortes.victorhugo@gmail.com>
2021-04-17 02:34:42 -0500

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,10 +9,15 @@ const startCase = (value) =>
     : "";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "555-1111" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const handleChange = (event) => setNewName(event.target.value);
+  const handleChangeName = (event) => setNewName(event.target.value);
+  const handleChangeNumber = (event) => setNewNumber(event.target.value);
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -20,7 +25,7 @@ const App = () => {
     const nameExist = persons.some(({ name }) => name === formatName);
     if (nameExist) return alert(`${formatName} is already added to phonebook}`);
 
-    setPersons([...persons, { name: formatName }]);
+    setPersons([...persons, { name: formatName, number: newNumber }]);
     setNewName("");
   };
 
@@ -29,7 +34,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleChangeName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleChangeNumber} />
         </div>
         <div>
           <button type="submit" disabled={!newName}>
@@ -38,8 +46,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name }) => (
-        <p key={name}>{name}</p>
+      {persons.map(({ name, number }) => (
+        <p key={name}>
+          {name} {number}
+        </p>
       ))}
     </div>
   );
